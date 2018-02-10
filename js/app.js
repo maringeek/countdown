86697f436fe983c6525f1f02932b934798b8f094
João Paulo Polles <joaopaulopolles@gmail.com>
2018-02-09 23:49:44 -0200

    colocado um adicional

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
+var gandalfAudio = getElementById("gandalf");
+var vitasAudio = getElementById("vitas");
+
 $(document).ready(function(){
   $("#counter").html("Carregando contador...");
   update();
+  $(document).keypress(function(k){
+    alert(k.witch);
+    if(k.witch == 18){
+      $("body").attr("id","vitas");
+      gandalfAudio.stop();
+      vitasAudio.play();
+    }
+  });
 });
 
 function update(){
-  $("#counter").html(countdown( new Date(2018, 0, 30, 12, 00, 0) ).toString());
+  $("#counter").html(countdown( new Date(2018, 4, 30, 12, 00, 0) ).toString());
   setTimeout(function(){ update(); }, 1000);
 }
