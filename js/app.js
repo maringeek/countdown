bdec568c4d6d7cc9636bb7ac362798e71dd03ec6
João Paulo Polles <joaopaulopolles@gmail.com>
2018-02-10 00:12:12 -0200

    teste do código

--- js/app.js
+++ js/app.js
@@ -2,17 +2,68 @@ var gandalfAudio = document.getElementById("gandalf");
 var vitasAudio = document.getElementById("vitas");
 
 $(document).ready(function(){
+  var etapa = 0;
   $("#counter").html("Carregando contador...");
   update();
   $(document).keypress(function(k){
-    alert(k.witch);
-    if(k.witch == 18){
-      $("body").attr("id","vitas");
-      gandalfAudio.stop();
-      vitasAudio.play();
+    switch(etapa){
+      case 0:
+      case 1:
+        if(k.witch == 38){
+          etapa++;
+        }
+        break;
+      case 2:
+      case 3:
+        if(k.witch == 40){
+          etapa++;
+        }else{
+          etapa = 0;
+        }
+        break;
+      case 4:
+      case 6:
+        if(k.witch == 37){
+          etapa++;
+        }else{
+          etapa = 0;
+        }
+        break;
+      case 5:
+      case 7:
+        if(k.witch == 39){
+          etapa++;
+        }else{
+          etapa = 0;
+        }
+        break;
+      case 8:
+        if(k.witch == 66){
+          etapa++;
+        }else{
+          etapa = 0;
+        }
+        break;
+      case 9:
+        if(k.witch == 65){
+          etapa++;
+          deuOCodigo();
+        }else{
+          etapa = 0;
+        }
+        break;
     }
   });
 });
+
+function deuOCodigo(){
+  alert(k.witch);
+  if(k.witch == 18){
+    $("body").attr("id","vitas");
+    gandalfAudio.stop();
+    vitasAudio.play();
+  }
+}
 
 function update(){
   $("#counter").html(countdown( new Date(2018, 4, 30, 12, 00, 0) ).toString());
